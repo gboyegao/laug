fcd83e5f43de544b9cc2b9281435e2c81e23d5a2
Adegboyega Olusunmade <gboyega@hey.com>
2024-11-03 17:43:34 +0000

    Create history.js
    For Amidats lesson

---
+++ js/history.js
@@ -0,0 +1,16 @@
+var quiz = {
+        title: 'HWA',
+        questions: [
+          {
+            text: "When did Queen Elizabeth 1 Rule?",
+            responses: [
+              {text: '1558 - 1603',value:'A'},
+              {text: '1907 - 1957',value:'B'},
+              {text: '2001 - 2016',value:'C'},
+              {text: '2020 - 2023',value:'D'}, 
+            ],
+              answer:'A'
+              
+          }
+        ]
+      };
